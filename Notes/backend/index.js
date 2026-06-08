e2b042cead25d21ead19c70bc76be3a6e5db4147
Alisa Mayes <alisamayes99@gmail.com>
2026-06-08 14:41:33 +0100

    Fixed a bug where the backend had no PUT route and made the toggle importance fail

--- Notes/backend/index.js
+++ Notes/backend/index.js
@@ -74,6 +74,21 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
+app.put('/api/notes/:id', (request, response) => {
+  const id = request.params.id
+  const body = request.body
+
+  const note = notes.find((note) => note.id === id)
+  if (!note) {
+    return response.status(404).end()
+  }
+
+  const updatedNote = { ...note, important: body.important, content: body.content }
+  notes = notes.map((note) => (note.id === id ? updatedNote : note))
+
+  response.json(updatedNote)
+})
+
 app.delete('/api/notes/:id', (request, response) => {
   const id = request.params.id
   notes = notes.filter((note) => note.id !== id)
