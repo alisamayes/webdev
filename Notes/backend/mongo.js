4c79edd961add788acfdf90353adcf29a8f1c669
Alisa Mayes <alisamayes99@gmail.com>
2026-06-09 15:52:02 +0100

    EOD Pushes, further work on render and mongodb

---
+++ Notes/backend/mongo.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://alisamayes_db_user:${password}@cluster0.ytqhhx3.mongodb.net/noteApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+/*
+const note = new Note({
+  content: 'Mäuschen wants Franzbrotchen',
+  important: true,
+})
+
+
+note.save().then(result => {
+  console.log('note saved!')
+  mongoose.connection.close()
+})
+*/
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
---
+++ Phonebook/backend/mongo.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 5) {
+  console.log('give password, name and number as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://alisamayes_db_user:${password}@cluster0.ytqhhx3.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url, { family: 4 })
+
+
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+  
+const User = mongoose.model('User', userSchema)
+
+const user = new User({
+    name: name,
+    number: number,
+})
+
+
+user.save().then(result => {
+  console.log('user saved!')
+  mongoose.connection.close()
+})
