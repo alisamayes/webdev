2c6d1bf2c60cfc7e08c70a3c31607200a76e4553
Alisa Mayes <alisamayes99@gmail.com>
2026-06-22 11:30:15 +0100

    bug fixes

--- Phonebook/backend/index.js
+++ Phonebook/backend/index.js
@@ -20,14 +20,19 @@ app.get('/api/persons', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
-  Person.findById(request.params.id)
-    .then(person => {
-      if (person) {
-        response.json(person)
-      } else {
-        response.status(404).end()
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  Person.findByIdAndUpdate(
+    request.params.id,
+    { name, number },
+    { new: true, runValidators: true, context: 'query' }
+  )
+    .then(updatedPerson => {
+      if (!updatedPerson) {
+        return response.status(404).end()
       }
+      response.json(updatedPerson)
     })
     .catch(error => next(error))
 })
