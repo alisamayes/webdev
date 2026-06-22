8f55759c73915909a10283211ebc49400d3b7730
Alisa Mayes <alisamayes99@gmail.com>
2026-06-22 11:18:17 +0100

    Updated notes to try and make work with render.com

--- Notes/backend/index.js
+++ Notes/backend/index.js
@@ -3,6 +3,8 @@ require('dotenv').config({ path: require('path').join(__dirname, '..', '.env') }
 const express = require('express')
 const Note = require('./models/note')
 
+const app = express()
+
 app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
@@ -37,7 +39,7 @@ app.get('/api/notes/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (!body.content) {
@@ -51,7 +53,7 @@ app.post('/api/notes', (request, response) => {
 
   note.save().then(savedNote => {
     response.json(savedNote)
-  })
+  }) .catch(error => next(error))
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
@@ -92,8 +94,10 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
-
+  }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
   next(error)
 }
 
